a6a61bf462f39aae619b421fa61755af6b2ed527
HanselRm <20220897@itla.edu.do>
2023-05-29 02:38:21 -0400

    f2

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 let input = document.getElementById("cajadetexto");
 let botones = document.querySelectorAll("button");
+let eliminarHistorial = document.getElementById("eliminarHistorial");
+let label = document.getElementById("historialtexto");
 
 let string = " ";
 let array = Array.from(botones);
+let historialGeneral = [];
+
+
+function guardarHistorial(historial){
+
+    historialGeneral.push(historial);
+    localStorage.setItem("historial", JSON.stringify(historialGeneral));
+
+    lst = localStorage.getItem("historial");
+    as = JSON.parse(lst);
+   return as;
+}   
+
+eliminarHistorial.addEventListener('click', (e) => {
+    localStorage.removeItem("historial");
+    label.innerHTML = " ";
+})
 
 array.forEach(boton => {
     boton.addEventListener('click', (e) => {
         if (e.target.innerHTML == '=') {
+            
+           let historialGuardado = guardarHistorial(string);
+
+           label.innerHTML = " ";
+           
             string = eval(string);
+           
+            for(var i = 0 ; i < historialGuardado.length; i++){
+                label.innerHTML += historialGuardado[i] + "<br>";
+               }
             input.value = string;
+         
         }
 
         else if(e.target.innerHTML == 'AC'){
@@ -23,10 +52,10 @@ array.forEach(boton => {
 
         else{
             string += e.target.innerHTML;
-            input.value = string;
+            input.value = string; 
         }
-
     })
+
 })
 
 
